fcabb7b3de2c2cb1a30193ee069d6cd3e242a656
aloyslili <AloysLi@163.com>
2022-07-20 16:41:42 +0800

    updata

---
+++ blog/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+
+  </div>
+</template>
+
+<script>
+  import AddMyblog from './components/AddMyblog'
+
+ export default {
+  name: 'app',
+  components: {
+    AddMyblog
+  }
+ }
+</script>
+<style lang="scss">
+
+</style>
